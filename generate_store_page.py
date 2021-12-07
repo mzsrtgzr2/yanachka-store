73efb8ab22aae10fc2779cc2da00992ccd8c1b8c
Moshe Rot <mrot@proofpoint.com>
2021-12-07 23:38:42 +0200

    v2

--- generate_store_page.py
+++ generate_store_page.py
@@ -1,15 +1,21 @@
 import json
 import urllib.request
-
+import re
 
 with open('./items.json', 'r') as fp:
     items = json.load(fp)
 
 with open('README.md', 'w') as fp:
+    fp.write('# החנות של ינהצ׳קה\n')
+
     for item in items:
         title = item.get('name')
         print('processing item', title)
         description = item.get('description')
+        if description:
+        #     description = re.sub(r'<p>(.*?)</p>', r'\n\0', description)
+            description = description.replace('<br/>', '\n')
+        print(description)
         image_to_download = item.get('imageUrl')
 
         image_name = image_to_download.split('/')[-1]
