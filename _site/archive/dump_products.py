c1f83c4cc9bd6ac1ad0ac92e1621d89df2c3119a
Your Name <you@example.com>
2023-03-28 11:17:19 +0300

    new site wip

---
+++ _site/archive/dump_products.py
@@ -0,0 +1,13 @@
+from pyecwid import Ecwid
+import json
+ecwid = Ecwid('xxx', '35000153')
+
+# Get all products and use lambda function to search results
+items = ecwid.products.get()
+print(json.dumps(items))
+
+
+
+
+
+
